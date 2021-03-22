bce30ab3c598db0c628a9f4b33bde2f3701a7fe3
Athilas-Silva <athilas.soares@hotmail.com>
2021-03-21 22:21:54 -0300

    Update index.js

--- src/Rotas/index.js
+++ src/Rotas/index.js
@@ -7,7 +7,7 @@ import Agenda from "../paginas/Agenda";
 import Missao from "../paginas/Missao";
 
 const Rotas = () => (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Switch>
             <Route exact path="/" component={ Home } />
             <Route exact path="/contato" component={ Contato } />
