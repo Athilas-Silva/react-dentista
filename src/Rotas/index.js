2846f901ba74a73f0d413cf0de9fb9a69eeb922b
Athilas-Silva <athilas.soares@hotmail.com>
2021-03-21 22:35:31 -0300

    Update index.js

--- src/Rotas/index.js
+++ src/Rotas/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { HashRouter, Route } from "react-router-dom";
 import Home from "../paginas/Home";
 import Contato from "../paginas/Contato";
 import Convenios from "../paginas/Convenios";
@@ -7,15 +7,13 @@ import Agenda from "../paginas/Agenda";
 import Missao from "../paginas/Missao";
 
 const Rotas = () => (
-    <BrowserRouter basename="/">
-        <Switch>
+    <HashRouter basename="/">
             <Route exact path="/" component={ Home } />
             <Route exact path="/contato" component={ Contato } />
             <Route exact path="/convenios" component={ Convenios } />
             <Route exact path="/agenda" component={ Agenda } />
             <Route exact path="/missao" component={ Missao } />
-        </Switch>
-    </BrowserRouter>
+    </HashRouter>
 );
 
 export default Rotas;
